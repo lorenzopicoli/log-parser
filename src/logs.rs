33a81573a9c6a90d9ec1c32d8df083c1859f7a67
Lorenzo Piccoli <lorenzopicoli@me.com>
2023-10-16 15:51:52 -0400

    first stable version

---
+++ src/logs.rs
@@ -0,0 +1,324 @@
+use std::collections::HashMap;
+
+use chrono::{DateTime, Local};
+use serde::{Deserialize, Serialize};
+use serde_json::{Map, Value};
+use serde_with::skip_serializing_none;
+use sqlformat::{Indent, QueryParams};
+use termcolor::Color;
+
+use crate::core::{FormattedLog, ParsableLog};
+
+/// Represents the internal data part of a standard log
+/// This is the trickiest part of the log. It can contain virtually anything
+/// And the data that's buried here is the most valuable for us (errors, who requested,
+/// body returned, etc)
+#[skip_serializing_none]
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct InternalDataLog {
+    internal_context: Option<HashMap<String, Value>>,
+    context: HashMap<String, Value>,
+    status_code: Option<i32>,
+    err: Option<HashMap<String, Value>>,
+    req: Option<HashMap<String, Value>>,
+    body: Option<HashMap<String, Value>>,
+    error: Option<HashMap<String, Value>>,
+    options: Option<HashMap<String, Value>>,
+    response: Option<HashMap<String, Value>>,
+}
+
+#[skip_serializing_none]
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct InternalLogResponse {
+    status_code: i32,
+    request: Option<HashMap<String, Value>>,
+    body: Option<HashMap<String, Value>>,
+    headers: Option<HashMap<String, Value>>,
+}
+
+/// A message can be either a string or an object. So we have 2 structs that we attempt to parse
+/// the message
+#[derive(Serialize, Deserialize, Debug)]
+struct StringMsgLog {
+    msg: String,
+}
+
+/// A message can be either a string or an object. So we have 2 structs that we attempt to parse
+/// the message
+#[derive(Serialize, Deserialize, Debug)]
+struct StringObjLog {
+    msg: HashMap<String, Value>,
+}
+
+/// Main log used by us. It contains detailed information that is useful at production envs, but
+/// that isn't necessarily full of useful information when developing. The way this works it'll
+/// "match" to any json object that contains an integer "level" property
+#[skip_serializing_none]
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct StandardLog {
+    level: u8,
+    // In ms
+    time: Option<u64>,
+    pid: Option<i32>,
+    hostname: Option<String>,
+    name: Option<String>,
+    data: Option<InternalDataLog>,
+
+    // Skip both msg and msg_obj as we'll manually try each one and populate accordingly
+    #[serde(skip, default = "default_none")]
+    msg: Option<String>,
+    #[serde(skip, default = "default_none")]
+    msg_obj: Option<HashMap<String, Value>>,
+}
+
+/// Implements the Parsable log trait for standardlog which ties our representation with the rest
+/// of the system and allows standard logs to be properly logged to the user using their
+/// configuration
+impl ParsableLog for StandardLog {
+    fn format_compact(&self) -> FormattedLog {
+        // We might have a time, but if something happens and we can't find it, use now
+        let time = match self.time {
+            Some(t) => DateTime::from_timestamp(t as i64 / 1000, 0)
+                .unwrap()
+                .with_timezone(&Local::now().timezone()),
+            None => Local::now(),
+        };
+
+        // Looks for a url in the request body. Useful to see exactly what url was called. So our
+        // log message could be `GET /users`, but we also want to log `GET /users?bla=xxx` and the
+        // easiest place to get that is in data.req.url
+        let url = match &self.data {
+            Some(d) => {
+                if let Some(req) = &d.req {
+                    req.get("url")
+                } else {
+                    None
+                }
+            }
+            None => None,
+        };
+
+        let mut log = "".to_string();
+        let mut extra: Option<HashMap<String, Value>> = None;
+        if let Some(url) = url {
+            log.push_str(url.as_str().unwrap_or(""));
+        }
+
+        if let Some(msg) = &self.msg {
+            log.push_str(&msg);
+        }
+
+        if let Some(msg) = &self.msg_obj {
+            log.push_str(&serde_json::to_string_pretty(&msg).unwrap_or("".to_string()));
+        }
+
+        // Useful for endpoints that respond with some sort of data in the body. This is usually
+        // were we log them
+        if let Some(data) = &self.data {
+            if let Some(body) = &data.body {
+                extra = Some(body.clone());
+                log.push_str(&" - Responding with");
+            }
+        }
+
+        FormattedLog {
+            date: time,
+            msg: log,
+            extra,
+            color_overwrite: None,
+        }
+    }
+
+    fn format_detailed(&self) -> FormattedLog {
+        let time = match self.time {
+            Some(t) => DateTime::from_timestamp(t as i64 / 1000, 0)
+                .unwrap()
+                .with_timezone(&Local::now().timezone()),
+            None => Local::now(),
+        };
+
+        // Looks for a url in the request body. Useful to see exactly what url was called. So our
+        // log message could be `GET /users`, but we also want to log `GET /users?bla=xxx` and the
+        // easiest place to get that is in data.req.url
+        let url = match &self.data {
+            Some(d) => {
+                if let Some(req) = &d.req {
+                    req.get("url")
+                } else {
+                    None
+                }
+            }
+            None => None,
+        };
+
+        let mut log = "".to_string();
+        let mut extra: HashMap<String, Value> = HashMap::new();
+        if let Some(url) = url {
+            log.push_str(url.as_str().unwrap_or(""));
+        }
+
+        if let Some(msg) = &self.msg {
+            log.push_str(&msg);
+        }
+
+        if let Some(msg) = &self.msg_obj {
+            log.push_str(&serde_json::to_string_pretty(&msg).unwrap_or("".to_string()));
+        }
+
+        // println!("What's available {:#?}", self);
+
+        // Useful for endpoints that respond with some sort of data in the body. This is usually
+        // were we log them
+        if let Some(data) = &self.data {
+            if let Some(internal_context) = &data.internal_context {
+                extra.insert("internalContext".to_string(), Value::Object(Map::new()));
+                for (k, v) in internal_context.iter() {
+                    extra
+                        .get_mut("internalContext")
+                        .unwrap()
+                        .as_object_mut()
+                        .unwrap()
+                        .insert(k.clone(), v.clone());
+                }
+            }
+            if let Some(req) = &data.req {
+                extra.insert("req".to_string(), Value::Object(Map::new()));
+                for (k, v) in req.iter() {
+                    extra
+                        .get_mut("req")
+                        .unwrap()
+                        .as_object_mut()
+                        .unwrap()
+                        .insert(k.clone(), v.clone());
+                }
+            }
+            if let Some(body) = &data.body {
+                extra.insert("dataBody".to_string(), Value::Object(Map::new()));
+                for (k, v) in body.iter() {
+                    extra
+                        .get_mut("dataBody")
+                        .unwrap()
+                        .as_object_mut()
+                        .unwrap()
+                        .insert(k.clone(), v.clone());
+                }
+            }
+        }
+
+        FormattedLog {
+            date: time,
+            msg: log,
+            extra: if extra.len() > 0 { Some(extra) } else { None },
+            color_overwrite: None,
+        }
+    }
+
+    fn from_line(line: &str) -> Option<Self> {
+        match serde_json::from_str::<StandardLog>(&line) {
+            Ok(mut standard_log) => {
+                // Standard log does not yet contain a message because it could be a string or an object. Let's try
+                // to parse that
+                if let Ok(str_msg) = serde_json::from_str::<StringMsgLog>(&line) {
+                    standard_log.msg = Some(str_msg.msg);
+                } else if let Ok(msg_obj) = serde_json::from_str::<StringObjLog>(&line) {
+                    standard_log.msg_obj = Some(msg_obj.msg);
+                }
+
+                Some(standard_log)
+            }
+            Err(_e) => {
+                // println!("ERRRRRR {}", e);
+                None
+            }
+        }
+    }
+}
+
+fn default_none<T>() -> Option<T> {
+    None
+}
+
+#[skip_serializing_none]
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct KnexLog {
+    sql: String,
+    bindings: Option<Value>,
+}
+impl ParsableLog for KnexLog {
+    fn format_compact(&self) -> FormattedLog {
+        let time = Local::now();
+        let bindings = self
+            .bindings
+            .clone()
+            .unwrap()
+            .as_array()
+            .unwrap()
+            .iter()
+            .map(|x| {
+                if x.is_string() {
+                    format!("'{}'", x.as_str().unwrap().to_string())
+                } else {
+                    x.to_string()
+                }
+            })
+            .collect();
+        let qs = QueryParams::Indexed(bindings);
+        let msg = sqlformat::format(
+            &self.sql.clone(),
+            &qs,
+            sqlformat::FormatOptions {
+                indent: Indent::Spaces(2),
+                uppercase: true,
+                lines_between_queries: 0,
+            },
+        );
+        FormattedLog {
+            date: time,
+            msg: format!(
+                "SQL query (might be different than the actual query):\n{}",
+                msg
+            ),
+            extra: None,
+            color_overwrite: Some(Color::Yellow),
+        }
+    }
+
+    fn from_line(line: &str) -> Option<Self> {
+        match serde_json::from_str::<KnexLog>(&line) {
+            Ok(r) => Some(r),
+            Err(_e) => {
+                // println!("ERRRRRR {}", e);
+                None
+            }
+        }
+    }
+
+    fn format_detailed(&self) -> FormattedLog {
+        let time = Local::now();
+        let msg = self.sql.clone();
+        let mut bindings_map: HashMap<String, Value> = HashMap::new();
+        if let Some(b) = &self.bindings {
+            bindings_map.insert("bidings".to_string(), b.clone());
+        }
+        FormattedLog {
+            date: time,
+            msg,
+            extra: Some(bindings_map),
+            color_overwrite: Some(Color::Blue),
+        }
+    }
+}
+
+pub fn try_parse_known_log(line: &str) -> Option<Box<dyn ParsableLog>> {
+    if let Some(v) = StandardLog::from_line(line) {
+        Some(Box::new(v))
+    } else if let Some(v) = KnexLog::from_line(line) {
+        Some(Box::new(v))
+    } else {
+        None
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,119 @@
-use std::io::{self, Write};
-use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
+use std::{
+    error::Error,
+    io::{self, BufRead, BufReader},
+    process::{ChildStdout, Command, Stdio},
+    sync::{Arc, Mutex},
+    thread,
+};
 
-struct InternalDataLog {
-    internal_context: serde_any_json_todo,
-    context: serde_anyjson_to,
-    err: serde_any_json_todo_or_string,
+use termcolor::Color;
+
+mod core;
+mod logs;
+
+#[derive(Debug)]
+enum FormatType {
+    Compact,
+    Detailed,
 }
 
-struct StandardLog {
-    level: u8,
-    time: i64,
-    pid: i32,
-    hostname: String,
-    name: String,
-    data: InternalDataLog,
-    msg: String,
-}
-// {"level":30,"time":1695749489844,"pid":55619,"hostname":"Lorenzos-MacBook-Pro.local","name":"[Worker-ID: 55619] - ","data":{"internalContext":{},"context":{}},"err":{},"msg":"HTTPClient - Executing request (GET) http://localhost:3013/groups/6664fe35-efa6-4a87-b69b-1fba30855453?"
-// }
-
-fn write_green() -> io::Result<()> {
-    let mut stdout = StandardStream::stdout(ColorChoice::Always);
-    stdout.set_color(ColorSpec::new().set_fg(Some(Color::Green)))?;
-    writeln!(&mut stdout, "green text!")
+#[derive(Debug)]
+struct CliState {
+    format_type: FormatType,
 }
 
-fn main() {
-    println!("Hello, world!");
-    write_green();
-    println!("Hello world 3");
+type Context = Arc<Mutex<CliState>>;
+
+fn read_and_parse_logs(mut reader: BufReader<ChildStdout>, context: Context) {
+    // Buffer that will hold lines as they come
+    let mut buffer = String::new();
+    // Keep trying to get a new line in a loop
+    loop {
+        // Try to read a new line
+        match reader.read_line(&mut buffer) {
+            Ok(_) => {
+                let line = buffer.as_str();
+
+                if line.len() == 0 {
+                    buffer.clear();
+                    continue;
+                }
+                // println!("{:#?}", context);
+                if let Some(log) = logs::try_parse_known_log(line) {
+                    let compact = match context.lock().unwrap().format_type {
+                        FormatType::Compact => log.format_compact(),
+                        FormatType::Detailed => log.format_detailed(),
+                    };
+                    compact.print();
+                } else {
+                    // Here we find unknown logs. Usually not formatted in JSON or quick
+                    // console.logs.
+                    // We should probably integrate this in parse_known_log and support other than
+                    // compact logs
+                    let compact = core::FormattedLog {
+                        date: chrono::Local::now(),
+                        msg: line.clone().to_string(),
+                        extra: None,
+                        color_overwrite: Some(Color::Blue),
+                    };
+                    compact.print();
+                }
+                buffer.clear()
+            }
+            Err(e) => {
+                // Stop listening if something happened. Shouldn't be called
+                println!("{}", e);
+                break;
+            }
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = std::env::args().collect();
+    let child_shell = Command::new(&args[1])
+        .args(&args[2..])
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+    let context: Context = Arc::new(Mutex::new(CliState {
+        format_type: FormatType::Detailed,
+    }));
+    let child_out = BufReader::new(child_shell.stdout.unwrap());
+
+    // So the main thread isn't hanging waiting for the server
+    let c = context.clone();
+    let child_thread = thread::spawn(move || read_and_parse_logs(child_out, c));
+
+    loop {
+        // Read user input
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        // Trim whitespace and newlines from the input
+        let input = input.trim();
+        if input.to_lowercase() == "exit" {
+            break;
+        }
+        if input.starts_with("logset:") {
+            match input {
+                "logset: compact" => {
+                    context.lock().unwrap().format_type = FormatType::Compact;
+                    println!("Setting mode to compact logs");
+                }
+                "logset: detailed" => {
+                    context.lock().unwrap().format_type = FormatType::Detailed;
+                    println!("Setting mode to detailed logs");
+                }
+                _ => println!("Unknown format"),
+            }
+            continue;
+        }
+    }
+
+    child_thread.join().unwrap();
+
+    Ok(())
 }
